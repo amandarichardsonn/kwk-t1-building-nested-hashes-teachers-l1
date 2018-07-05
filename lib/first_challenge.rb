3ef024ba5f6a26beec686451d3fdbe1cb7ec14b1
Amanda Richardson <amanda162000@gmail.com>
2018-07-05 15:50:03 +0000

    automatically backed up by learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -1,5 +1,6 @@
-def first_challenge
-   epic_tragedy = {:montague => {
+
+   epic_tragedy = {
+      :montague => {
       :patriarch => {name: "Lord Montague", age: "53"},
       :matriarch => {name: "Lady Montague", age: "54"},
       :hero => {name: "Romeo", age: "15", status: "alive"},
@@ -18,4 +19,3 @@ def first_challenge
       ]
    }
  }
-end
